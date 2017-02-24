ebc7388f24d77520a706c4a200bf7baeaeddc233
subhankar29 <subhankar29shah@gmail.com>
2017-02-24 23:08:24 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/subhankar29/crimeawareness/TakePhoto.java
@@ -0,0 +1,13 @@
+package com.example.subhankar29.crimeawareness;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TakePhoto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_take_photo);
+    }
+}
