73346ce6fa7f7ae98851461ade216741fce356fc
mayank-verma048 <mayank.verma048@gmail.com>
2017-02-25 15:27:11 +0530

    Merge Conflict Solve

--- app/src/main/java/com/example/subhankar29/crimeawareness/TakePhoto.java
+++ app/src/main/java/com/example/subhankar29/crimeawareness/TakePhoto.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.database.FirebaseDatabase;
