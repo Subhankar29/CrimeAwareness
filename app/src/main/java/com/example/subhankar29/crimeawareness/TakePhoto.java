5b04f54473023ebd70234bbb5102428ab7ada123
subhankar29 <subhankar29shah@gmail.com>
2017-02-25 14:06:50 +0530

    Added Time Picker, subject and description

--- app/src/main/java/com/example/subhankar29/crimeawareness/TakePhoto.java
+++ app/src/main/java/com/example/subhankar29/crimeawareness/TakePhoto.java
@@ -2,12 +2,39 @@ package com.example.subhankar29.crimeawareness;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 
 public class TakePhoto extends AppCompatActivity {
-
+    Button time;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_photo);
+        final TextView time  = (TextView) findViewById(R.id.time);
+        time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                //get current date time with Date()
+                Date date = new Date();
+                System.out.println(dateFormat.format(date));
+
+                //get current date time with Calendar()
+                Calendar cal = Calendar.getInstance();
+                System.out.println(dateFormat.format(cal.getTime()));
+                time.setText((CharSequence) cal.getTime());
+            }
+        });
+
     }
+
+
 }
